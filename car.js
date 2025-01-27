3fde2dca71dcb1766d131834e0a5a9f0d43e523a
charlesassoi <charlesassoi07@gmail.com>
2025-01-27 23:14:14 +0000

    new

--- car.js
+++ car.js
@@ -1,10 +1,24 @@
-class Car{
+import { Controls} from './controls.js';
+
+
+export class Car{
     constructor(x,y,width,height){
         this.x=x;
         this.y=y;
         this.width=width;
         this.height=height;
+
+        this.controls=new Controls();
     }
+    update(){
+        if(this.controls.forward){
+            this.y-=2;
+        }
+        if(this.controls.reverse){
+            this.y+=2;
+        }
+    }
+    
     draw(ctx){
         ctx.beginPath();
         ctx.rect(
@@ -16,3 +30,4 @@ class Car{
         ctx.fill();
     }
 }
+
--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const { JSDOM } = require("jsdom");
-
-// Simuler un DOM virtuel
-const { document } = new JSDOM('<!DOCTYPE html><html><body><div id="grid"></div></body></html>').window;
-
+import { Car} from './car.js';
+import { Controls} from './controls.js';
 
 const canvas = document.getElementById("myCanvas");
 canvas.height=window.innerHeight;
 canvas.width=200;
+
+
 const ctx=canvas.getContext("2d");
 const car=new Car(100,100,30,50);
-car.draw(ctx);
+
+
+animate();
+function animate(){
+    car.update();
+    car.draw(ctx);
+}

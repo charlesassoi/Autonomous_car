22b017c8ee4d289e5009c2fb9310e9b8426a4a48
charlesassoi <charlesassoi07@gmail.com>
2025-01-28 10:09:42 +0000

    new

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { Car} from './car.js';
 import { Controls} from './controls.js';
 
 const canvas = document.getElementById("myCanvas");
-canvas.height=window.innerHeight;
 canvas.width=200;
 
 
@@ -13,5 +12,7 @@ const car=new Car(100,100,30,50);
 animate();
 function animate(){
     car.update();
+    canvas.height=window.innerHeight;
     car.draw(ctx);
+    requestAnimationFrame(animate);
 }

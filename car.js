25c5487475afb1731008507aa2aeb3187b0ef243
charlesassoi <charlesassoi07@gmail.com>
2025-01-28 13:48:23 +0000

    new

--- car.js
+++ car.js
@@ -36,13 +36,18 @@ export class Car{
         if(Math.abs(this.speed)<this.friction){
             this.speed=0;
         }
+        if(this.speed!=0){
+            const flip=this.speed>0?1:-1;
+        
         if(this.controls.left){
-            this.angle+=0.03;
+            this.angle+=0.03*flip;
         }
         if (this.controls.right){
-            this.angle -=0.03;
+            this.angle -=0.03*flip;
         }
-        this.y-=this.speed;
+        }
+        this.x-=Math.sin(this.angle)*this.speed;
+        this.y-=Math.cos(this.angle)*this.speed;
     }
     
     draw(ctx){

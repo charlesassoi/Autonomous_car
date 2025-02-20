d999e39d12853e6a8d8403f927b90e9d097d1148
charlesassoi <charlesassoi07@gmail.com>
2025-02-20 13:36:10 +0000

    new

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ animate();
 function animate(){
     car.update();
     canvas.height=window.innerHeight;
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
     road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
